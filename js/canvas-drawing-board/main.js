dedf4f41b1b25c5f1019dc55313c4a57b8699418
luyi <luyi95@foxmail.com>
2020-12-10 15:28:59 +0800

    make a drawing board(only support draw lines) with canvas api

--- js/canvas-drawing-board/main.js
+++ js/canvas-drawing-board/main.js
@@ -1,26 +1,47 @@
+let drawingBoard = document.getElementById('drawingBoard');
+drawingBoard.width = window.innerWidth;
+drawingBoard.height = window.innerHeight;
+let ctx = drawingBoard.getContext('2d');
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 2;
+ctx.lineCap = 'round';
+
 let canDraw = false;
-    canvas.onclick = (e) => {
-      console.log('click:', e.clientX, e.clientY);
+let previousPosition;
+
+let isTouchDevice = 'ontouchstart' in document.documentElement;
+
+console.log('isTouchDevice = ', isTouchDevice);
+
+if (isTouchDevice) {
+  drawingBoard.ontouchstart = (e) => {
+    previousPosition = [e.touches[0].clientX, e.touches[0].clientY]
+  }
+  drawingBoard.ontouchmove = (e) => {
+    drawLine(previousPosition[0], previousPosition[1], e.touches[0].clientX, e.touches[0].clientY);
+  }
+} else {
+  drawingBoard.onmousedown = (e) => {
+    console.log('begin');
+    previousPosition = [e.clientX, e.clientY];
+    canDraw = true;
+  }
+  drawingBoard.onmousemove = (e) => {
+    if (canDraw) {
+      drawLine(previousPosition[0], previousPosition[1], e.clientX, e.clientY);
     }
-    canvas.onmousedown = (e) => {
-      console.log('down');
-      canDraw = true;
-    }
-    canvas.onmousemove = (e) => {
-      if (canDraw) {
-        console.log('drawing')
-        let dot = document.createElement('div');
-        dot.style.position = 'absolute';
-        dot.style.left = e.clientX + 'px';
-        dot.style.top = e.clientY + 'px';
-        dot.style.width = '5px';
-        dot.style.height = '5px';
-        dot.style.backgroundColor = 'black';
-        dot.style.borderRadius = '50%';
-        canvas.appendChild(dot);
-      }
-    }
-    canvas.onmouseup = (e) => {
-      console.log('up');
-      canDraw = false;
-    }
+  }
+  drawingBoard.onmouseup = (e) => {
+    console.log('end');
+    canDraw = false;
+  }
+}
+
+function drawLine(previousX, previousY, currentX, currentY) {
+  console.log('drawing...')
+  ctx.beginPath();
+  ctx.moveTo(previousX, previousY);
+  ctx.lineTo(currentX, currentY);
+  ctx.stroke();
+  previousPosition = [currentX, currentY];
+}
